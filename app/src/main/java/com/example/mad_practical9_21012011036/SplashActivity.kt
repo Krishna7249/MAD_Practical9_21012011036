c7a2bc176e9d442cbbb500ad4410525be9a000f7
Krishna7249 <98690735+Krishna7249@users.noreply.github.com>
2023-10-18 21:25:15 +0530

    pr

--- app/src/main/java/com/example/mad_practical9_21012011036/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical9_21012011036/SplashActivity.kt
@@ -1,26 +1,45 @@
 package com.example.mad_practical9_21012011036
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
 import android.widget.ImageView
+import android.view.animation.AnimationUtils
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), AnimationListener {
     lateinit var uvpcelogoanimation : AnimationDrawable
+    lateinit var twinAnimation: Animation
+    lateinit var img: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val img = findViewById<ImageView>(R.id.uvpce_logo)
+        img = findViewById<ImageView>(R.id.uvpce_logo)
         img.setBackgroundResource(R.drawable.uvpce_animation_list)
         uvpcelogoanimation = img.background as AnimationDrawable
+        twinAnimation = AnimationUtils.loadAnimation(this, R.anim.twin_animation)
+        twinAnimation.setAnimationListener(this)
     }
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus){
             uvpcelogoanimation.start()
+            img.startAnimation(twinAnimation)
         }
         else{
             uvpcelogoanimation.stop()
         }
     }
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+    override fun onAnimationEnd(p0: Animation?) {
+        Intent(this,MainActivity::class.java).apply { startActivity(this) }
+    }
+    override fun onAnimationRepeat(p0: Animation?){
+
+    }
+
 }
